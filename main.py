23455834a20d5509be949afadb6173172048b268
Rutuparn <rutuparn.17U253@viit.ac.in>
2020-10-02 14:19:10 +0530

    Minor changes

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ if __name__ == '__main__':
 	print("Select operation:\n1- Encrypt\n2- Decrypt")
 	choice = int(input("Enter choice:"))
 	if choice == 1:
-		print(cipher.encrypt(str(input("Enter text:"))))
+		print(cipher.encrypt(str(input("Enter text: "))))
 	elif choice == 2:
-		print(cipher.decrypt(str(input("Enter encrypted text"))))
+		print(cipher.decrypt(str(input("Enter encrypted text: "))))
 	else:
 		print("Invalid choice")
